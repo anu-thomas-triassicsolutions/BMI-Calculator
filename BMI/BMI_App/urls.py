0b6502e607ce04cf1a6e0678e2699a72de1d89cb
anu-thomas-triassicsolutions <107909020+anu-thomas-triassicsolutions@users.noreply.github.com>
2022-09-13 10:27:49 +0530

    last updation

--- BMI/BMI_App/urls.py
+++ BMI/BMI_App/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path("bmi_calculator/", views.bmi_calculator, name="bmi_calculator"),  # BMI Calculator
     path("bmi_list/", views.bmi_list, name="bmi_list"),  # BMI List
+    path('plot/', views.plot, name='plot')  # Histogram based on category
 ]
